e2390a1e2c910a679eace17fa0e48a9b8261555c
MestreCaio10 <caiofeliciocobra@gmail.com>
2026-09-20 14:45:04 -0300

    update

--- Dia1/Exercises/01.js
+++ Dia1/Exercises/01.js
@@ -2,4 +2,4 @@ const name="Caio";
 const city="Franca";
 const age=22;
 
-console.log("Eu moro na cidade:", city, "me chamo ", name, "e tenho ", age, "anos." )
+console.log("Eu moro na cidade:", city, "me chamo ", name, "e tenho ", age, "anos" )
